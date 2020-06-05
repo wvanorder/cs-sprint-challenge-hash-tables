354f9ed4948bc5a2dbc32eeb71da90bd0ba2bb6b
Will VanOrder <william.vanorder94@gmail.com>
2020-06-05 09:37:18 -0800

    4/5 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,25 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for i in range(length):
+        cache[weights[i]] = i
+
+    for weight in weights:
+        pair = limit - weight
+        if pair not in cache:
+            continue
+        else:
+            if weight > pair:
+                return [cache[weight], cache[pair]]
+            else:
+                return [cache[pair], cache[weight]]
+
 
     return None
+
+weights = [ 4, 6, 10, 15, 16 ]
+length = 5
+limit = 21
+
+get_indices_of_item_weights(weights, 5, 21)
